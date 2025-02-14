363004dcde88229d75c1180eaa9747967ecda385
LakshyaTangri <lakshyatangri1@gmail.com>
2025-02-14 20:44:09 +0000

    RF Model

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 
+import pandas as pd
+from electricity_load_predictor import ElectricityLoadPredictor
+# Load your data
+df = pd.read_csv('data\supermarket_sales.csv')
+
+# Initialize the predictor
+predictor = ElectricityLoadPredictor()
+
+# Preprocess data
+X, y, feature_columns = predictor.preprocess_data(df)
+
+# Train the model and get evaluation metrics
+results = predictor.train(X, y)
+
+# Make predictions on new data
+new_predictions = predictor.predict('data\supermarket_sales.csv')
